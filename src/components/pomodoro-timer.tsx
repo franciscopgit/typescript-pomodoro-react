726e65e0a8169d2d2b9897ded066fc634dc875ed
Francisco Pereira <franciscopereiraadm6@gmail.com>
2023-10-14 18:42:53 -0300

    372
    Botão para iniciar o Pomodoro (Working)

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
 // import { secondsToTime } from '../utils/seconds-to-time';
 import { Button } from './botton';
@@ -15,6 +15,10 @@ export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.PomodoroTime);
   const [timeCounting, setTimeCounting] = React.useState(false);
   const [working, setWorking] = React.useState(false);
+
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
 
   useInterval(
     () => {
@@ -34,8 +38,11 @@ export function PomodoroTimer(props: Props): JSX.Element {
       <Timer mainTime={mainTime} />
       <div className="controls">
         <Button text="Work" onClick={() => configureWork()}></Button>
-        <Button text="Pause" onClick={() => console.log('2')}></Button>
         <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
       </div>
       <div className="details">
         <p>Testando: Lorem ipsum dolor, sit amet consectetur.</p>
