d8f47cb91f9424b3accc1858c7f0bb677a8d87b4
Francisco Pereira <franciscopereiraadm6@gmail.com>
2023-10-14 18:26:38 -0300

    setTimeCounting & work
    criando a função de work e setTimeconfig

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -13,18 +13,28 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.PomodoroTime);
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
-        <Button text="teste" onClick={() => console.log(1)}></Button>
-        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="Work" onClick={() => configureWork()}></Button>
+        <Button text="Pause" onClick={() => console.log('2')}></Button>
         <Button text="teste" onClick={() => console.log(1)}></Button>
       </div>
       <div className="details">
